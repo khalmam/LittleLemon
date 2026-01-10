82949618ca91ce35163c58f1e2cbd857cbbfa18f
khalmam <167781390+khalmam@users.noreply.github.com>
2026-01-10 06:32:55 +0100

    Add pymysql for MySQL database support

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -15,7 +15,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+import pymysql
 
+pymysql.install_as_MySQLdb()
 load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
